d4cd6f9e893cbb40a3f237762673fb4fa6ccd09a
eduardo <caetasousa@gmail.com>
2021-08-27 18:09:12 -0300

    test

--- models.py
+++ models.py
@@ -12,7 +12,3 @@ class Hero(SQLModel, table=True):
     team_id: Optional[int] = Field(default=None, foreign_key="team.id")
 
 
-class Team(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
-    headquarters: str
