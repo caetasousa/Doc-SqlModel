f5672e83ed60da49dce62ee1fa7c978877f0b85d
eduardo <caetasousa@gmail.com>
2021-08-27 21:37:15 -0300

    Atributos de relacionamento

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ def create_heroes():
         team_z_force = Team(name="Z-Force", headquarters="Sister Margaret’s Bar")
         session.add(team_preventers)
         session.add(team_z_force)
-        session.commit()
 
-        hero_deadpond = Hero(name="Deadpond", secret_name="Dive Wilson", team_id=team_z_force.id)
-        hero_rusty_man = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48, team_id=team_preventers.id)
+        hero_deadpond = Hero(name="Deadpond", secret_name="Dive Wilson", team=team_z_force)
+        hero_rusty_man = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48, team=team_preventers)
         hero_spider_boy = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+        hero_tarantula = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
+        hero_dr_weird = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
+        hero_cap = Hero(name="Captain North America", secret_name="Esteban Rogelios", age=93)
 
         session.add(hero_deadpond)
         session.add(hero_rusty_man)
@@ -29,33 +31,46 @@ def create_heroes():
         print("Created hero:", hero_rusty_man)
         print("Created hero:", hero_spider_boy)
 
+        hero_spider_boy.team = team_preventers 
+        session.add(hero_spider_boy)
+        session.commit()
+        session.refresh(hero_spider_boy)
+        print("Updated hero:", hero_spider_boy)
+
+        team_preventers.heroes.append(hero_tarantula)
+        team_preventers.heroes.append(hero_dr_weird)
+        team_preventers.heroes.append(hero_cap)
+        session.add(team_preventers)
+        session.commit()
+        session.refresh(hero_tarantula)
+        session.refresh(hero_dr_weird)
+        session.refresh(hero_cap)
+        print("Preventers new hero:", hero_tarantula)
+        print("Preventers new hero:", hero_dr_weird)
+        print("Preventers new hero:", hero_cap)
+
 
 def select_heroes():
     with Session(engine) as session:
-        #statement = select(Hero, Team).where(Hero.team_id == Team.id) # faz a mesma coisa que utilizando o join
-        #statement = select(Hero, Team).join(Team, isouter=True) # isouter=True diz para trazer o que tiver team_id=Null
-        statement = select(Hero, Team).join(Team).where(Team.name == "Preventers") # nao junta as 2 tabelas for com 2 items
-        #statement = select(Hero).join(Team).where(Team.headquarters == "Sharp Tower") # depois de fazer o join, cria uma unica tabela e tbm e possivel colocar filtros o for e com 1 elemento
-        results = session.exec(statement)
-        for hero, team in results:
-            print("Hero:", hero, "Team:", team)
-        #for hero in results:
-            #print("Preventer Hero:", hero)
+        statement = select(Team).where(Team.name == "Preventers")
+        result = session.exec(statement)
+        team_preventers = result.one()
+
+        print("Preventers heroes:", team_preventers.heroes) # acessa os heroes atraves da busca de Team
 
 
 def update_heroes():
     with Session(engine) as session:
-        heroi = select(Hero).where(Hero.secret_name == 'Pedro Parqueador')
-        time = select(Team).where(Team.name == "Preventers")
-        resultsheroi = session.exec(heroi).one()
-        resultstime = session.exec(time).one() # para descobri qual e o id do time
-        
-        resultsheroi.team_id = None # tirando a relacao com o time Preventers
-        session.add(resultsheroi)
-        session.commit()
-        session.refresh(resultsheroi)
+        statement = select(Hero).where(Hero.name == "Spider-Boy")
+        result = session.exec(statement)
+        hero_spider_boy = result.one()
 
-        print("Updated hero:", resultsheroi)
+        hero_spider_boy.team = None # encerra a ligacao da tabela atraves do team e nao do team_id
+        session.add(hero_spider_boy)
+        session.commit()
+
+        session.refresh(hero_spider_boy)
+        print("Spider-Boy without team:", hero_spider_boy)
 
 
 def create_db_and_tables():
--- models.py
+++ models.py
@@ -1,6 +1,14 @@
-from typing import Optional
+from typing import List, Optional
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
+
+
+class Team(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    headquarters: str
+
+    heroes: List["Hero"] = Relationship(back_populates="team")
 
 
 class Hero(SQLModel, table=True):
@@ -10,9 +18,6 @@ class Hero(SQLModel, table=True):
     age: Optional[int] = None
 
     team_id: Optional[int] = Field(default=None, foreign_key="team.id")
+    team: Optional[Team] = Relationship(back_populates="heroes") # Team tem q vir primeiro q Hero
 
 
-class Team(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
-    headquarters: str
